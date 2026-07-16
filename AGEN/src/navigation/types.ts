ffd593731fa1b813a0b3e2d1ddcd334b5988a09d
Joy Saha <studentroutinemanager@gmail.com>
2026-07-16 22:21:37 +0600

    Stop tracking AGEN and hotel folders

--- AGEN/src/navigation/types.ts
+++
@@ -1,9 +0,0 @@
-export type AuthStackParamList = {
-  Login: undefined;
-  SignUp: undefined;
-  ForgotPassword: undefined;
-  OtpVerification: {
-    email: string;
-    mode: "login" | "signup" | "recovery";
-  };
-};
